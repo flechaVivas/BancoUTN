fd5463d7c4f1ea2babf45dc4c1a646c1a9f61638
flechaVivas <facundovivas15@gmail.com>
2022-05-22 10:19:26 -0300

    Ventana login deplegable

--- src/ui/Login.java
+++ src/ui/Login.java
@@ -1,9 +1,52 @@
 package ui;
 
-public class Login {
+import java.awt.Color;
+import java.awt.Dimension;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class Login extends JFrame {
 	
+	JPanel panel = new JPanel();
+	JLabel usuario = new JLabel();
+	JLabel contrasenia = new JLabel();
+	JTextField user = new JTextField();
+	JPasswordField passwd = new JPasswordField();
+	JButton boton = new JButton();
 	
+	public Login() {
+		
+		this.setSize(500, 500);
+		this.setLocationRelativeTo(null); 
+		
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		this.setTitle("Login Page");
+		
+		this.setMinimumSize(new Dimension(200,200));
+		
+		
+		iniciarComponentes();
+	}
+	
+	private void iniciarComponentes() {
+		
+		panel.setBackground(new Color(122,108,240)); 
+		
+		this.getContentPane().add(panel);
+		
+		panel.setLayout(null); 
+		
+		
+		
+		
+		
+	}
 	
 	
 }
--- src/ui/Principal.java
+++ src/ui/Principal.java
@@ -4,8 +4,8 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
-		
-
+		Login l = new Login();
+		l.setVisible(true);
 	}
 
 }
